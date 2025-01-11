e264454bc4d2131159280ddc02f5fd1e1248cf13
Dragontamer7866 <tacoboycharles@gmail.com>
2025-01-11 11:36:09 -0600

    Update rickroll.py

--- rickroll.py
+++ rickroll.py
@@ -1,7 +1,8 @@
 import time
 import webbrowser
 
-tabs = 0
-while tabs < 3:
+timer = 0
+while timer < 3:
     time.sleep(1)
     webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+    timer = timer + 1
