42e06f04cda20c19f5c538d1e0494ade2a790d89
Dragontamer7866 <tacoboycharles@gmail.com>
2025-05-12 13:32:16 -0500

    gyat

--- rickroll.py
+++ rickroll.py
@@ -2,30 +2,31 @@ import time
 import webbrowser
 
 version = input("Would you like the Normal, Meow Meow, or Villager covers? ")
+count = int(input("How many tabs? "))
 if version == "Normal":
     timer = 0
-    while timer < 3:
+    while timer < count:
         time.sleep(1)
         webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
         timer = timer + 1
 
 elif version == "Meow Meow":
     timer = 0
-    while timer < 3:
+    while timer < count:
         time.sleep(1)
         webbrowser.open("https://www.youtube.com/watch?v=dmA6_0ZwWb4")
         timer = timer + 1
 
 elif version == "Villager":
     timer = 0
-    while timer < 3:
+    while timer < count:
         time.sleep(1)
         webbrowser.open("https://www.youtube.com/watch?v=2GXlAJPa-KM")
         timer = timer + 1
 
 else:
     timer = 0
-    while timer < 3:
+    while timer < count:
         time.sleep(1)
         webbrowser.open("https://youtu.be/2U4E-RXHshc?=VMqvKCvt3M4fQ-OO")
         timer = timer + 1
